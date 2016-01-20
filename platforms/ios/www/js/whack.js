af51a3ebdc8a6d8468c296622d8fec06b9247051
mattheh <mattheh@email.unc.edu>
2016-01-20 13:36:40 -0500

    Add Grass Image

--- platforms/ios/www/js/whack.js
+++ platforms/ios/www/js/whack.js
@@ -17,9 +17,14 @@ var app = {
 
 app.initialize();
 
+var bgImage = new Image();
+bgImage.src = 'img/grass.jpg';
+
 function moleHole(x,y){
 	this.x = x;
 	this.y=y;
+        this.width = window.innerWidth/3;
+        this.height = window.innerHeight/3;
 	var tempImage = new Image();
 	tempImage.src = 'img/mole_hole.png';
 	this.img = tempImage;
@@ -35,10 +40,14 @@ var moleArr = []
 
 for (i = 0; i < 3; i++)
 	for(j=0;j <3;j++)
-		moleArr.push(new moleHole(i*(window.innerWidth/2 -20),j*(window.innerHeight/2 -20)))
+		moleArr.push(new moleHole(i*(window.innerWidth/3),j*(window.innerHeight/3)))
 function render(){		
+	ctx.font = "18px Helvetica";
+        ctx.strokeStyle = "white";
+        ctx.drawImage(bgImage,0,0,window.innerWidth,window.innerHeight)
 	for(i=0; i < 9; i++){
-		ctx.drawImage(moleArr[i].img,moleArr[i].x,moleArr[i].y)
+        	ctx.drawImage(moleArr[i].img,moleArr[i].x,moleArr[i].y,moleArr[i].width, moleArr[i].height)
+	        ctx.strokeText("password " + i, moleArr[i].x, moleArr[i].y + moleArr[i].height/2);
 	}
 	requestAnimationFrame(render)
 }
